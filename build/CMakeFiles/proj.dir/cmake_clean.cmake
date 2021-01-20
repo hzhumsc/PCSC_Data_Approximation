093d10a0debcf60fe8e8df18c4fb7e4a6bfbe092
Haojun Zhu <haojun.zhu@epfh.ch>
2021-01-20 22:09:25 +0100

    check push before modifying the readme

---
+++ build/CMakeFiles/proj.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj.dir/Approximator.cpp.o"
+  "CMakeFiles/proj.dir/Data.cpp.o"
+  "CMakeFiles/proj.dir/Matrix.cpp.o"
+  "CMakeFiles/proj.dir/UserInterface.cpp.o"
+  "CMakeFiles/proj.dir/main.cpp.o"
+  "CMakeFiles/proj.dir/test/GeneralTest.cpp.o"
+  "CMakeFiles/proj.dir/test/Test_1.cpp.o"
+  "CMakeFiles/proj.dir/test/Test_2.cpp.o"
+  "CMakeFiles/proj.dir/test/Test_3.cpp.o"
+  "CMakeFiles/proj.dir/test/Test_4.cpp.o"
+  "proj"
+  "proj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
